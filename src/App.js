9d54ebb7f7f2bfc37f2948ac945f27a0cbe333c6
Sam Kahr <samkahr@gmail.com>
2018-11-20 11:46:41 +0400

    adding code for todo components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+
+
+
+
   render() {
     return (
       <div className="App">
-      <ul>
-          <ToDo />
+      <ul style={{liststyletype: "none"}}>
+          <ToDo  />
           <ToDo />
         </ul>
       </div>
